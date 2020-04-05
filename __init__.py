c72fdc4c436ba4592a3f6c42ca46618763967347
luca-vercelli <luca.vercelli.to@gmail.com>
2020-04-05 22:32:21 +0200

    day 1

--- __init__.py
+++ __init__.py
@@ -2,29 +2,30 @@ from mycroft import MycroftSkill, intent_file_handler
 
 import subprocess
 
+SEARCH_ENGINE = "http://www.duckduckgo.com"
+
 class OpenPrograms(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    SEARCH_ENGINE="http://www.duckduckgo.com"
     @intent_file_handler('programs.open.intent')
     def handle_programs_open(self, message):
         """
         This whould work with a few programs, such as Firefox and Thunderbird
         """
-        program_name = message.data.get('programname')
+        name = message.data.get('program')
         try:
-            subprocess.run([program_name])
-            self.speak_dialog('programs.open', {'programname' : program_name})
+            subprocess.run([program])
+            self.speak_dialog('programs.open', {'program' : program})
         except:
-            self.speak_dialog('programs.open.notfound', {'programname' : program_name})
+            self.speak_dialog('programs.open.notfound', {'program' : program})
 
     @intent_file_handler('programs.open.browser.intent')
     def handle_programs_open(self, message):
         subprocess.run(["xdg-open", SEARCH_ENGINE], check=True)
         self.speak_dialog('programs.open')
 
-    def stop():
+    def stop(self):
         # should close active program ?!?
         pass
 
